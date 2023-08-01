2642883513c40477f6a80d7cd7a68abf7bd1f63d
AlexisOl170818 <alexizde555@gmail.com>
2023-08-01 16:45:48 -0600

    first version with gridView

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italiana',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Rapido & Facil',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburguesas',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Alemana',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Ligera & deliciosa',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotica',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Almuerzos',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asiatica',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Verano',
+    color: Colors.teal,
+  ),
+];
